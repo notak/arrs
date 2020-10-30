b7e034e6314f3bec3e01fd2c5bbcee339733595c
Chris Stafford <i@taks.me>
2020-10-30 22:28:11 +0000

    refactor to tidy up. do some testing to confirm that tiplocs should be passed as strings when gzipped

---
+++ src/test/java/utils/encoders/MoniousTest.java
@@ -0,0 +1,23 @@
+package utils.encoders;
+
+import org.junit.Test;
+
+public class MoniousTest {
+	@Test
+	private static void integrity() {
+//		var spec = spec((byte) 5, (byte) 35, (byte)-12, (short)0, Bytes.toArray((byte)0, (byte)-2, (byte)1), Freight.NONE, (byte)3, (byte)0, (byte)0);
+////		var spec = new Spec(new Spec.Struct()).withCategory((byte)5);
+//		var encoded = spec.monious();
+//		printf("Encoded \"%s\" \"%s\"\n", bytesToHex(encoded), paddedBinary(encoded));
+//		var spec2 = Spec.fromMonious(encoded);
+//		printf("Integrity %b\n %s\n %s\n", spec.equals(spec2), spec, spec2);
+//
+//		var buf = new byte[10000];
+//		int[] pos = { 434 };
+//		MoniousParser.varInt(buf, pos, 12332);
+//		pos[0] = 434;
+//		var zd = MoniousParser.unVarInt(buf, pos);
+//		printf("VarInt integrity: %b %d %d\n", zd==12332, 12332, zd);
+	}
+
+}
